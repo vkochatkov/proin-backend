df5edc71605bae77cc6d9dccfbc185c791aa0362
vkochatkov <vlad.kochatkov@gmail.com>
2023-03-31 17:00:22 +0300

    change variables for vercel

--- controllers/projects-controllers.js
+++ controllers/projects-controllers.js
@@ -10,9 +10,9 @@ const AWS = require('aws-sdk');
 require('dotenv').config();
 
 const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3 = new AWS.S3({
   region, 
