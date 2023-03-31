84ba2b949455ffebdfa1e38f462a8a9c59d96fc6
vkochatkov <vlad.kochatkov@gmail.com>
2023-03-31 17:35:44 +0300

    update vercel file

--- app.js
+++ app.js
@@ -28,17 +28,6 @@ const corsOptions = {
   
   app.use(cors(corsOptions))
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
-//   next();
-// });
-
 app.use('/projects', projectsRoutes);
 app.use('/users', usersRoutes);
 
